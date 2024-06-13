480e11f55996a4c7560d6bfbd47b3528ea56ad90
Sebastian Müller <sebastian.mueller@syskron.com>
2024-06-13 12:42:47 +0200

    Adding security group for inbound and outbound communication

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -41,5 +42,40 @@ resource "aws_default_route_table" "main-rtb" {
   }
   tags = {
     Name: "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  # we just open one specific port, but we could also open from port 1 to 1000 e.g.
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+
+  # 0.0.0.0/0 means any ip address can reach our application
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # installing tools like docker on the server needs access to the internet for downloading binaries etc.
+  # port 0 means every port and protocol -1 means every protocol
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-sg"
   }
 }
