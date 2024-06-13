bc8a392bef809accdc64e53da3d2df5d58c069a3
Sebastian Müller <sebastian.mueller@syskron.com>
2024-06-13 12:18:50 +0200

    add internet gateway and routing table for internet traffic

--- main.tf
+++ main.tf
@@ -14,11 +14,32 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
+resource "aws_subnet" "myapp-subnet-1" {
   vpc_id = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
     Name: "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  # we don't have to define the route for the internal communcation
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-rtb"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name: "${var.env_prefix}-igw"
   }
 }
